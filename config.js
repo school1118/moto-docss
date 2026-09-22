12e4db53c82a7e7475fcdce81b00210141ac3e6e
school1118 <ahmadali22024@gmail.com>
2026-09-22 15:13:03 +0330

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// ============================================
-//   تنظیمات پروژه — این فایل را پر کن
-// ============================================
-
-window.SUPABASE_URL = 'https://vljyxznauhrknazinzop.supabase.co';
-window.SUPABASE_ANON_KEY = 'sb_publishable_x3y3k-cU317WrjkfQSdSkA_06o3CRJd';  // ← رشته کامل را اینجا بگذار
-
-window.APP_PASSWORD = 'moto1404';  // ← رمز دلخواه ورود
